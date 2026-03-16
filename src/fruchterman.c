6cf7c9ce6fbca3132423587920dfcc9e89168f6c
lter-p <andrewvdovichenko86@gmail.com>
2026-03-16 23:05:07 +0100

    Changed spectral algorithm to fruchterman's

---
+++ src/fruchterman.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "../include/graph.h"
+#include "../include/fruchterman.h"
+
+int use_fruchterman_for_graph(const Edge *initial_graph, Node *output_graph)
+{
+    return 0;
+}
--- src/spectral.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "../include/graph.h"
-#include "../include/spectral.h"
-
-int use_spectral_for_graph(const Edge *initial_graph, Node *output_graph)
-{
-    return 0;
-}
