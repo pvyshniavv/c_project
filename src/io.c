25ab08784ddb0f81522409aae86e380283a11b84
pvyshniavv <01205793@pw.edu.pl>
2026-04-11 00:01:42 +0200

    correcting some mistakes in function and ending the deserialize_file function

--- src/io.c
+++ src/io.c
@@ -2,6 +2,7 @@
 #include "command.h"
 #include "graph.h"
 #include "io.h"
+#include "error_handler.h"
 
 /**
  * @brief function checks if the file is valid
@@ -12,17 +13,49 @@
 
 int deserialize_file(char *filename, Edge *elements)
 {
-    FILE *f = fopen( filename, "r");
-    if(f == NULL) 
-    perror ("Brak pliku.");
-    return 0;
 
-    if(scanf(filename, "%lf", &elements) != 1) {
-        fclose(filename);
-        return 0;
+    if (filename == NULL || elements == NULL) {
+        return ERROR_INVALID_PARAMETER; //using error 1 from error_handler
     }
 
+    //openin file in reading mode
+    FILE *f = fopen( filename, "r");
+    if(f == NULL) {
+    perror ("Brak pliku.");
+    return 0; 
 }
+
+
+    Edge *current = elements;
+
+    char temp_name[3];
+    unsigned int temp_start, temp_end;
+    double temp_wieght;
+
+    //creating buffer for saving text-line form file
+    char buffer[256];
+
+    while(fgets(buffer, sizeof(buffer), f) != NULL) {
+
+        if(sscanf(buffer, "%2s %u %u %lf", temp_name, &temp_start, &temp_end, &temp_weight) == 4) {
+
+            Edge *new_edge = (Edge*)malloc(sizeof(Edge));
+            if(new_edge == NULL) {
+                fclose(f);
+                return ERROR_INVALID_PARAMETER;
+            }
+
+            
+        }
+    }
+
+
+}
+
+
+
+
+
 
 int create_output_file(enum file_output_type format, const Node *elements, char *output_file_path)
 {
