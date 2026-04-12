308e16b3e4c222a567b97bc638ff6137e0f7144d
pvyshniavv <01205793@pw.edu.pl>
2026-04-12 17:03:25 +0200

    adding some commentaries for deserialize_file function

--- src/io.c
+++ src/io.c
@@ -13,32 +13,36 @@
 
 int deserialize_file(char *filename, Edge *elements)
 {
-
+    //checking if pointers are not empty 
     if (filename == NULL || elements == NULL) {
         return ERROR_INVALID_PARAMETER; //using error 1 from error_handler
     }
 
-    //openin file in reading mode
+    //opening file in a reading mode
     FILE *f = fopen( filename, "r");
     if(f == NULL) {
     perror ("Brak pliku.");
-    return ERROR_NO_INPUT_FILE_PATH_WAS_GIVEN; 
+    return ERROR_NO_INPUT_FILE_PATH_WAS_GIVEN; //using error 20 from error_handler
 }
 
 
     Edge *current = elements;
 
+    //setting temporary methods
     char temp_name[3];
     unsigned int temp_start, temp_end;
     double temp_weight;
 
-    //creating buffer for saving text-line form file
+    //creating buffer for saving text-line from file
     char buffer[256];
 
+    //we read the file over lines
     while(fgets(buffer, sizeof(buffer), f) != NULL) {
 
+        //We try to extract 4 variables from the loaded line using sscanf
         if(sscanf(buffer, "%2s %u %u %lf", temp_name, &temp_start, &temp_end, &temp_weight) == 4) {
 
+            //the allocation of memory, only when thw line was correct
             Edge *new_edge = (Edge*)malloc(sizeof(Edge));
             if(new_edge == NULL) {
                 fclose(f);
@@ -52,6 +56,7 @@ int deserialize_file(char *filename, Edge *elements)
             new_edge->weight = temp_weight;
             new_edge->next = NULL;
 
+            //attaching the node to the list
             current->next = new_edge;
             current = new_edge;
 
@@ -59,7 +64,7 @@ int deserialize_file(char *filename, Edge *elements)
     }
 
     fclose(f);
-    return 0;
+    return 0; //if everything ended succesfully 
 
 }
 
