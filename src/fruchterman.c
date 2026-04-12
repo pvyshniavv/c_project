31b645d42a5b44f25e79ba38b59210a4cf250731
pvyshniavv <01205793@pw.edu.pl>
2026-04-12 21:43:14 +0200

    corrected small mistakes, finished fruchterman algotithm

--- src/fruchterman.c
+++ src/fruchterman.c
@@ -10,10 +10,11 @@ static double calculate_distance(double x1, double x2, double y1, double y2) {
 
 //the main algorythm
 int use_fruchterman_for_graph(const Edge *initial_graph, Node *output_graph) {
+    //checking if pointers are not empty 
     if(initial_graph == NULL || output_graph == NULL)
     return -1;
 
-    //evaluate the number of nodes in list
+    //evaluating the number of nodes in list
     int num_nodes = 0;
     Node *curr_node = output_graph;
     while( curr_node != NULL) {
@@ -23,7 +24,7 @@ int use_fruchterman_for_graph(const Edge *initial_graph, Node *output_graph) {
 
     if (num_nodes == 0) return 0;
 
-    //creation of temporary errays for faster calculations
+    //created temporary errays for faster calculations
     double *x = (double *)calloc(num_nodes, sizeof(double));
     double *y = (double *)calloc(num_nodes, sizeof(double));
     double *dx = (double *)calloc(num_nodes, sizeof(double));
@@ -62,7 +63,7 @@ int use_fruchterman_for_graph(const Edge *initial_graph, Node *output_graph) {
             double delta_x = x[v] - x[u];
             double delta_y = y[v] - y[u];
             double dist = calculate_distance(x[v], x[u], y[v], y[u]);
-            if(dist < 0,0001) dist = 0.0001;
+            if(dist < 0.0001) dist = 0.0001;
 
             double force = (k * k) / dist; //repulsive force by two nodes
             dx[v] += (delta_x / dist) * force; 
@@ -76,11 +77,13 @@ int use_fruchterman_for_graph(const Edge *initial_graph, Node *output_graph) {
             unsigned int u = curr_edge->start_node;
             unsigned int v = curr_edge->end_node;
 
+
+            //making sure we don't do out of array bounds
             if(u < (unsigned int)num_nodes && v < (unsigned int)num_nodes) {
             double delta_x = x[u] - x[v];
             double delta_y = y[u] - y[v];
             double dist = calculate_distance(x[v], x[u], y[v], y[u]);
-            if(dist < 0,0001) dist = 0.0001;
+            if(dist < 0.0001) dist = 0.0001;
 
             double force = (dist * dist) / k; //gravitation force by two nodes
             double disp_x = (delta_x / dist) * force; 
@@ -94,8 +97,38 @@ int use_fruchterman_for_graph(const Edge *initial_graph, Node *output_graph) {
             curr_edge = curr_edge->next;
         }
 
-        // the 3 step: Using force
-        
+        // the 3 step: Using force (updating positions)
+        for(int v = 0; v< num_nodes; v++) {
+            double disp_length = sqrt(dx[v] * dx[v] + dy[v] * dy[v]);
 
+            if(disp_length < 0.0001) disp_length = 0.0001;
+            double move_x = (dx[v] / disp_length) * fmin(disp_length, temperature);
+            double move_y = (dy[v] / disp_length) * fmin(disp_length, temperature);
+
+            x[v] += move_x;
+            y[v] += move_y;
+
+            if (x[v] < 0.0) x[v] = 0.0;
+            if (x[v] > width) x[v] = width;
+            if (y[v] > height) y[v] = height;
+            if (y[v] < 0.0) y[v] = 0.0;
+
+            temperature *= 0.95;
+        }
+        
+        //the 5 step:  transfer calculated data back to the linked list
+        curr_node = output_graph;
+        int i = 0;
+        while(curr_node != NULL) {
+            curr_node->x_axis = x[i];
+            curr_node->y_axis = y[i];
+            curr_node = curr_node->next;
+            i++;
+        }
+        free(x);
+        free(y);
+        free(dx);
+        free(dy);
     }
+    return 0;
 }
