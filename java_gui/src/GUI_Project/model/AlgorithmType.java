35aca79d536f3eee416b8e78adb719f89797b8c7
pvyshniavv <01205793@pw.edu.pl>
2026-06-04 22:28:42 +0200

    AlgorythmType.java has implemented

--- java_gui/src/GUI_Project/model/AlgorithmType.java
+++ java_gui/src/GUI_Project/model/AlgorithmType.java
@@ -1,6 +1,24 @@
 package GUI_Project.model;
 
-    public enum AlgorithmType {
-        Tutte,
-        SpectralLayout
+public enum AlgorithmType {
+
+    TUTTE("Tutte", "--tutte"),
+    SPECTRAL_LAYOUT("SpectralLayout", "--spectral");
+
+    private final String displayName;
+    private final String flag;
+
+    AlgorithmType(String displayName, String flag) {
+        this.displayName = displayName;
+        this.flag = flag;
     }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
+}
