5c2b2fc501a3b3132eb50b3e84a978cc33c582ee
pvyshniavv <01205793@pw.edu.pl>
2026-06-04 23:32:03 +0200

    ProcessingConfig.java file ended, comments added

--- java_gui/src/GUI_Project/model/ProcessingConfig.java
+++ java_gui/src/GUI_Project/model/ProcessingConfig.java
@@ -1,25 +1,67 @@
 package GUI_Project.model;
 
+/**
+ * Configuration holder. Stores file paths and the runtime parameters
+ * chosen by the user before launching the external C module.
+ */
 public class ProcessingConfig {
-    private String inputFileName;
-    private String outputFileName;
-    private AlgorithmType algorithmUsed;
-    private FileFormat outputFileFormat;
 
-    public ProcessingConfig(String inputFileName, String outputFileName, AlgorithmType algorithmUsed, FileFormat outputFileFormat) {
-        this.inputFileName = inputFileName;
-        this.outputFileName = outputFileName;
-        this.algorithmUsed = algorithmUsed;
-        this.outputFileFormat = outputFileFormat;
+    private String inputPath;
+    private String outputPath;
+    private AlgorithmType algorithm = AlgorithmType.TUTTE;
+    private FileFormat outputFormat = FileFormat.BINARY;
+
+    /** Number of clusters the graph should be split into (must be >= 1). */
+    private int clusterCount = 2;
+
+    /** Allowed imbalance margin between clusters (must be >= 0). */
+    private double margin = 0.0;
+
+    public String getInputPath() {
+        return inputPath;
     }
 
-    public String getInputFileName() { return inputFileName; }
-    public String getOutputFileName() { return outputFileName; }
-    public AlgorithmType getAlgorithmUsed() { return algorithmUsed; }
-    public FileFormat getOutputFileFormat() { return outputFileFormat; }
+    public void setInputPath(String inputPath) {
+        this.inputPath = inputPath;
+    }
 
-    public void setInputFileName(String inputFileName) { this.inputFileName = inputFileName; }
-    public void setOutputFileName(String outputFileName) { this.outputFileName = outputFileName; }
-    public void setAlgorithmUsed(AlgorithmType algorithmUsed) { this.algorithmUsed = algorithmUsed; }
-    public void setOutputFileFormat(FileFormat outputFileFormat) { this.outputFileFormat = outputFileFormat; }
+    public String getOutputPath() {
+        return outputPath;
+    }
+
+    public void setOutputPath(String outputPath) {
+        this.outputPath = outputPath;
+    }
+
+    public AlgorithmType getAlgorithm() {
+        return algorithm;
+    }
+
+    public void setAlgorithm(AlgorithmType algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    public FileFormat getOutputFormat() {
+        return outputFormat;
+    }
+
+    public void setOutputFormat(FileFormat outputFormat) {
+        this.outputFormat = outputFormat;
+    }
+
+    public int getClusterCount() {
+        return clusterCount;
+    }
+
+    public void setClusterCount(int clusterCount) {
+        this.clusterCount = clusterCount;
+    }
+
+    public double getMargin() {
+        return margin;
+    }
+
+    public void setMargin(double margin) {
+        this.margin = margin;
+    }
 }
