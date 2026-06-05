b12da05e85f15da984873a309449317ed8abf13f
pvyshniavv <01205793@pw.edu.pl>
2026-06-05 19:21:43 +0200

    ProcessingException.java has been added and implemented

---
+++ java_gui/src/GUI_Project/integration/ProcessingException.java
@@ -0,0 +1,12 @@
+package GUI_Project.integration;
+
+public class ProcessingException extends Exception {
+
+    public ProcessingException(String message) {
+        super(message);
+    }
+
+    public ProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
