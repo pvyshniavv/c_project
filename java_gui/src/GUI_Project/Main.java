6033b31876f5bcfeb5cb715af98ece24aa4a90ed
pvyshniavv <01205793@pw.edu.pl>
2026-06-05 22:05:26 +0200

    Main.java has been implemented with some comments

--- java_gui/src/GUI_Project/Main.java
+++ java_gui/src/GUI_Project/Main.java
@@ -1,4 +1,28 @@
 package GUI_Project;
 
+import GUI_Project.integration.CExternalProcessor;
+import GUI_Project.integration.GraphProcessor;
+import GUI_Project.io.GraphFileReader;
+import GUI_Project.view.MainFrame;
+import javax.swing.SwingUtilities;
+import java.io.File;
+
+/**
+ * Application entry point. Hands control to the Event Dispatch Thread (EDT)
+ * and creates the presentation-layer objects, wiring the MVP triad together.
+ */
 public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Main::startApplication);
+    }
+    private static void startApplication() {
+        // Path to the compiled C module. Adjust to your build output, e.g.
+        // ../c_backend/graph_tool  (or graph_tool.exe on Windows).
+        File cExecutable = new File("../c_backend/graph_tool");
+
+        MainFrame frame = new MainFrame();
+        GraphFileReader reader = new GraphFileReader();
+        GraphProcessor processor = new CExternalProcessor(cExecutable);
+
+    }
 }
